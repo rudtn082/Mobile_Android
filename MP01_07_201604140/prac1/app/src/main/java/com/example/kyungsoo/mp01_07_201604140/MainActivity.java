388bec2fcf9805737daaa3e8e99d518bf14e7cf4
KyungSoo <kyung5587@gmail.com>
2018-11-13 13:29:41 +0900

    2번까지 저장

---
+++ MP01_07_201604140/prac1/app/src/main/java/com/example/kyungsoo/mp01_07_201604140/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kyungsoo.mp01_07_201604140;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
