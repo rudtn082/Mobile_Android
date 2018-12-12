c1b7ef5fe80382ed3bef735d7b82b7fd13260a61
rudtn082 <rudtn082@naver.com>
2018-12-13 02:31:39 +0900

    7번까지

---
+++ MP01_10_201604140/prac5/app/src/main/java/org/techtown/mp01_10_201604140/MainActivity.java
@@ -0,0 +1,28 @@
+package org.techtown.mp01_10_201604140;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button playButton = (Button) findViewById(R.id.play);
+        playButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent =new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse("http://sites.google.com/site/ubiaccessmobile/sample_audio.amr");
+                intent.setDataAndType(uri, "audio/amr");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ MP01_10_201604140/prac7/app/src/main/java/org/techtown/mp01_10_201604140/MainActivity.java
@@ -0,0 +1,29 @@
+package org.techtown.mp01_10_201604140;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.net.Uri;
+import android.os.Environment;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+
+        VideoView videoview = (VideoView)this.findViewById(R.id.videoview);
+        MediaController mc = new MediaController(this);
+        videoview.setMediaController(mc);
+        String folder = Environment.getExternalStorageDirectory().getAbsolutePath() + "/download/trailer.mp4";
+        Log.e("asd", folder);
+
+        videoview.setVideoPath(folder);
+        videoview.requestFocus();
+        videoview.start();
+    }
+}
